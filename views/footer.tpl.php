09dad575591047fed824da5d4fc28d3447bf7d7c
tsmith <tsmith@blinds.com>
2009-04-10 12:20:50 -0500

    Add tags for XHTML 1.0 Strict and CSS validation.

--- views/footer.tpl.php
+++ views/footer.tpl.php
@@ -1,5 +1,15 @@
         </div>
         <div id="footer">
+            <p>
+                <a href="http://validator.w3.org/check?uri=referer"><img
+                   src="http://www.w3.org/Icons/valid-xhtml10-blue"
+                   alt="Valid XHTML 1.0 Strict" height="31" width="88" style="border: 0" /></a>
+                <a href="http://jigsaw.w3.org/css-validator/check/referer">
+                    <img style="border:0;width:88px;height:31px"
+                         src="http://jigsaw.w3.org/css-validator/images/vcss-blue"
+                         alt="Valid CSS!" />
+                </a>
+            </p>
         </div>
         <div id="top_nav">
             <ul>
