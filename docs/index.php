f619af2442c36c6d98ad4984fd95cf19ed593eeb
tsmith <tsmith@blinds.com>
2009-04-27 17:22:13 -0500

    Brochure index.

---
+++ docs/index.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title>User Directory Documents</title>
+        <style type="text/css">
+.box { float: left; border: 2px solid black; padding: 15px; background: white; margin-right: 20px; }
+h1 { margin-top: 0; text-align: center; }
+        </style>
+    </head>
+    <body style="background: #eee">
+        <div class="box">
+            <h1>Entity Relationship Diagram (ERD)</h1>
+            <div style="text-align: center">
+                <img src="db_model.png" alt="database model" width="414" height="274"/>
+            </div>
+        </div>
+        <div class="box">
+            <h1>Mockup</h1>
+            <div style="text-align: center">
+                <img src="portal.png" alt="Mockup"/>
+            </div>
+        </div>
+    </body>
+</html>
