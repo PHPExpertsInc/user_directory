5b64a6a645e91a082dc24b2e802ac132f6216bb7
hopeseekr <hopeseekr@xmule.ws>
2009-05-01 13:14:09 -0500

    Added SecurityController test.

---
+++ tests/SecurityControllerTest.php
@@ -0,0 +1,88 @@
+<?php
+
+require_once 'managers/UserManager.inc.php';
+require_once 'controllers/SecurityController.inc.php';
+
+require_once 'PHPUnit/Framework/TestCase.php';
+
+/**
+ * SecurityController test case.
+ */
+class SecurityControllerTest extends PHPUnit_Framework_TestCase {
+	
+	/**
+	 * Prepares the environment before running a test.
+	 */
+	protected function setUp()
+	{
+		session_start();
+		parent::setUp();
+	}
+	
+	/**
+	 * Cleans up the environment after running a test.
+	 */
+	protected function tearDown()
+	{
+		session_destroy();
+		parent::tearDown();
+	}
+	
+	/**
+	 * Constructs the test case.
+	 */
+	public function __construct()
+	{
+	}
+	
+	/**
+	 * Tests SecurityController::isLoggedIn()
+	 * 
+	 * @covers SecurityController::isLoggedIn
+	 */
+	public function testIsLoggedIn()
+	{
+		// 1. Test with no input
+		$this->assertFalse(SecurityController::isLoggedIn(), 'worked with no input.');
+		
+		// 2. Test with bad input
+		$_SESSION['userInfo'] = uniqid();
+		$this->assertFalse(SecurityController::isLoggedIn(), 'worked with bad input.');
+		
+		// 3. Test with good input
+		// Simulate login
+		$_SESSION['userInfo'] = new UserInfoStruct;
+		$this->assertTrue(SecurityController::isLoggedIn(), 'didn\'t work with good input.');
+	}
+
+	/**
+	 * Tests SecurityController::ensureHasAccess()
+	 * 
+	 * @covers SecurityController::ensureHasAccess
+	 */
+	public function testEnsureHasAccess()
+	{
+		// 1. Test while not being logged in
+		try
+		{
+			SecurityController::ensureHasAccess();
+			$this->assertTrue(false, 'worked without being logged in.');
+		}
+		catch(Exception $e)
+		{
+			$this->assertEquals(UserManager::NOT_LOGGED_IN, $e->getCode(), 'Didn\'t expect exception "' . $e->getMessage() . '".');
+		}
+		
+		$headers_list = headers_list();
+		if (!empty($headers_list))
+		{
+			$this->assertContains('Location: http://' . $_SERVER['HTTP_HOST'] . '/user_directory/', $headers_list);
+		}
+		
+		// 2. Test while being logged in
+		$_SESSION['userInfo'] = new UserInfoStruct;
+		$this->assertEquals(null, SecurityController::ensureHasAccess());
+	}
+	
+}
+
--- tests/UserDirectoryTests.php
+++ tests/UserDirectoryTests.php
@@ -4,6 +4,7 @@ require_once 'PHPUnit/Framework/TestSuite.php';
 
 require_once 'tests/MyDatabaseTest.php';
 require_once 'tests/SearchControllerTest.php';
+require_once 'tests/SecurityControllerTest.php';
 require_once 'tests/UserControllerTest.php';
 require_once 'tests/UserManagerTest.php';
 
@@ -20,14 +21,8 @@ class UserDirectoryTests extends PHPUnit_Framework_TestSuite {
 		ob_start();
 		$this->setName ( 'UserDirectoryTests' );
 		$this->addTestSuite('MyDatabaseTest');
-/*
-		// Load PDO
-		$db = array('host' => 'localhost', 'user' => 'ud_tester', 'pass' => 'PXhu6u6-)', 'db' => 'TEST_user_directory');
-		$dsn = sprintf('mysql:dbname=%s;host=%s;', $db['db'], $db['host']);
-        $pdo = new PDO($dsn, $db['user'], $db['pass']);        
-        getDBHandler($pdo);
-*/		
 		$this->addTestSuite('UserManagerTest');
+		$this->addTestSuite('SecurityControllerTest');
 		$this->addTestSuite('UserControllerTest');
 		$this->addTestSuite('SearchControllerTest');
 		PHPUnit_Util_Filter::removeDirectoryFromWhitelist('tests');
