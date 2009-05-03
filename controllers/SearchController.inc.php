a9bbb7447ab247962d6cb969a9fadd3072c69711
hopeseekr <hopeseekr@xmule.ws>
2009-05-02 19:06:27 -0500

    Fixes Search Controller.

--- controllers/SearchController.inc.php
+++ controllers/SearchController.inc.php
@@ -22,30 +22,28 @@ class SearchController
 	{
 		SecurityController::ensureHasAccess();
 
-		if (!isset($_POST['search']))
+		if (!isset($_POST['search']) && !isset($_GET['page']))
 		{
 			return false;
 		}
 
 		/* --- Filter user input --- */
-		$searchParams['username'] = strip_tags($_REQUEST['username']);
-		$searchParams['firstName'] = strip_tags($_REQUEST['firstName']);
-		$searchParams['lastName'] = strip_tags($_REQUEST['lastName']);
-		$searchParams['email'] = strip_tags($_REQUEST['email']);
-
+		$searchParams['username'] = isset($_REQUEST['username']) ? strip_tags($_REQUEST['username']) : '';
+		$searchParams['firstName'] = isset($_REQUEST['firstName']) ? strip_tags($_REQUEST['firstName']) : '';
+		$searchParams['lastName'] = isset($_REQUEST['lastName']) ? strip_tags($_REQUEST['lastName']) : '';
+		$searchParams['email'] = isset($_REQUEST['email']) ? strip_tags($_REQUEST['email']) : '';
 		$this->searchParams = $searchParams;
-		//$this->searchParams['active'] = 1;
 
 		return $this->userManager->searchUsers($searchParams);
 	}
 
 	public function getSearchQueryString()
 	{
-		if (isset($_POST['search']) || isset($_GET['active']))
+		if (isset($_POST['search']) || isset($_GET['page']))
 		{
 			return http_build_query($this->searchParams);
 		}
 		
-			return null;
+	        return null;
 	}
 }
