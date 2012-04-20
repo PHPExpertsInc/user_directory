0fa9cdd87a1eeb6a01b11f72ecb4aa0bc0d83584
Theodore R. Smith <theodore@phpexperts.pro>
2012-04-20 14:46:17 -0500

    Changes to get phpunit.xml to work.

--- tests/bootstrap.inc.php
+++ tests/bootstrap.inc.php
@@ -3,14 +3,8 @@
 ob_start();
 
 require_once realpath(dirname(__FILE__) . '/../lib/bootstrap.inc.php');
-require_once 'PHPUnit/Framework/TestSuite.php';
+//require_once 'PHPUnit/Framework/TestSuite.php';
 
 
 require_once APP_PATH . '/lib/MyDB.inc.php';
-
-require_once APP_PATH . '/tests/ControllerCommandTest.php';
-require_once APP_PATH . '/tests/MyDatabaseTest.php';
-require_once APP_PATH . '/tests/SearchControllerTest.php';
-require_once APP_PATH . '/tests/SecurityControllerTest.php';
-require_once APP_PATH . '/tests/UserControllerTest.php';
-require_once APP_PATH . '/tests/UserManagerTest.php';
+require_once APP_PATH . '/lib/UserInfoStruct.inc.php';
